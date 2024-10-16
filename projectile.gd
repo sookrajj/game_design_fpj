89a448a54f54f376ce2e2568ead14c9d76fa2f19
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2024-10-16 14:26:21 -0500

    hi

--- projectile.gd
+++ projectile.gd
@@ -19,15 +19,12 @@ func _physics_process(delta: float) -> void:
 	self.move_local_x(speed*dir)
 	if position.x < spawnPos.x-100 || position.x > spawnPos.x+100:
 		queue_free()
-		print("check")
 	var velocity = Vector2(speed * dir, 0)
 	
 
 func _on_body_entered(body: Node) -> void:
-	print("new")
 	if body.is_in_group("enemies"):
 		body.queue_free();
-		print("hi")
 
 func set_spawn(pos : Vector2) :
 	spawnPos = pos
