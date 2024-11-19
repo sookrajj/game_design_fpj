90cb86f10effc16a976f38773454ad2da03316f3
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2024-11-19 14:34:45 -0600

    hi

--- level_transfer.gd
+++ level_transfer.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 @export var next_level = ""
-
+@export var coorx = 0
+@export var coory = 0
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
@@ -11,6 +12,10 @@ func _on_body_entered(body: Node2D) -> void:
 			var level = "res://" + next_level + ".tscn"
 			var plar = body.data
 			
+			#get_tree().change_scene_to_file(level)
 			get_tree().change_scene_to_file(level)
-			var player = get_tree().get_nodes_in_group("Player")
+			#for player in get_tree().get_nodes_in_group("Player"):
+				#if player != null:
+			var player = get_tree().get_first_node_in_group("Player")
 			player.data.max_health = plar.max_health
+					#player.global_position = player.global_position + Vector2(coorx, coory)
